a0699c02235bfe82342afaaf36ad12788a64ac5a
Vladimir <diffdiff@yandex.ru>
2022-11-05 00:10:21 +0300

    Диплом промежуток -ревью

--- statistics/src/main/java/ru/practicum/explorewithme/exceptions/ApiError.java
+++ statistics/src/main/java/ru/practicum/explorewithme/exceptions/ApiError.java
@@ -1,30 +1,22 @@
-package ru.practicum.explorewithme.exceptions;
+    package ru.practicum.explorewithme.exceptions;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import ru.practicum.explorewithme.exceptions.StatusError;
+    import com.fasterxml.jackson.annotation.JsonFormat;
+    import lombok.AllArgsConstructor;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+    import lombok.Setter;
+    import java.time.LocalDateTime;
+    import java.util.List;
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class ApiError {
+        private List<String> errors;
+        private String message;
+        private String reason;
+        private StatusError status;
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime timestamp;
 
-import java.time.LocalDateTime;
-import java.util.List;
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ApiError {
-    private List<String> errors;
-    private String message;
-    private String reason;
-    private StatusError status;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime timestamp;
-
-//    try{
-//        if(run.run(i, us, adminPermission))runer = true;
-//    }catch (java.lang.Throwable e){
-//        e.printStackTrace();
-//    }
-
-}
+    }
